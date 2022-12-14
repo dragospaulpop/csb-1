274729835d6943ad83e4aebe2a471c1f96864acc
Dragos-Paul Pop <dragos_paul_pop@yahoo.com>
2022-12-14 19:22:11 +0200

    cleanup

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page that is loaded dynamically</h1>
   </div>
 </template>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,15 @@
+<template>
+  <index-page />
+</template>
+
+<script>
+import IndexPage from '../components/IndexPage'
+
+export default {
+  name: 'IndexView',
+
+  components: {
+    IndexPage
+  }
+}
+</script>
